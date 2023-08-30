d6f456a870409ca9688b5dae6bee6bb42874b9ad
João Lenon <lenonSec7@gmail.com>
2023-08-30 18:10:35 +0100

    feat(command): add build command

--- bin/artisan.ts
+++ bin/artisan.ts
@@ -25,6 +25,8 @@ process.env.IS_TS = 'true'
 await Config.loadAll(Path.stubs('config'))
 
 Config.delete('app.version')
+Config.set('rc.build.tsconfig', './tests/stubs/tsconfig.json')
+Config.set('rc.build.metaFiles', ['app/hello.edge', 'LICENSE.md'])
 Config.set('rc.meta', import.meta.url)
 Config.set('logging.channels.console.driver', 'console')
 Config.set('logging.channels.exception.driver', 'console')
